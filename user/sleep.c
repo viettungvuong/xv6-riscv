0fd387b7513ac5a027e3a131594ab7997b91b942
viettungvuong <viettungvuong@gmail.com>
2024-01-25 08:36:44 +0700

    Edit error exit

--- user/sleep.c
+++ user/sleep.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         printf("error: incorrect number of arguments\n");
-        exit(0);
+        exit(1);
     }
 
     int time = atoi(argv[1]);
