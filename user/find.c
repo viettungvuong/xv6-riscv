c273c7ab9d1c1afbdf1aade9d6608dc41f3268d9
viettungvuong <viettungvuong@gmail.com>
2024-02-22 00:21:59 +0800

    find

--- user/find.c
+++ user/find.c
@@ -3,8 +3,6 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-// '.' Matches any single character.​​​​
-// '*' Matches zero or more of the preceding element.
 int match(char *s, char *p)
 {
     if (!*p)
@@ -15,88 +13,90 @@ int match(char *s, char *p)
         return *s == *p || (*p == '.' && *s != '\0') ? match(s, p + 2) || match(s + 1, p) : match(s, p + 2);
 }
 
-void find(int fin, char *dir, char *name)
+void find(char *path, char *filename)
 {
+    char current[512];
+    int fd;
     struct dirent de;
-    char path[512];
-
-    while (read(fin, &de, sizeof(de)) == sizeof(de))
-    {
-        if (de.inum == 0) // empty slot
-            continue;
-        if (!strcmp(de.name, ".") || !strcmp(de.name, ".."))
-            continue;
-
-        memcpy(path, dir, strlen(dir));
-        char *p = path + strlen(dir);
-        *p++ = '/';
-        memcpy(p, de.name, strlen(de.name));
-        p += strlen(de.name);
-        *p++ = 0;
-
-        struct stat st;
-
-        // check if the file is a directory
-        if (stat(path, &st) < 0)
-        {
-            printf("find: No stat %s\n", path);
-            continue;
-        }
-        if (st.type == T_FILE && match(de.name, name))
-        {
-            printf("%s\n", path);
-        }
-        else if (st.type == T_DIR)
-        {
-            int sub = open(path, 0);
-            if (sub < 0)
-            {
-                printf("find: Can't open %s\n", path);
-                continue;
-            }
-            find(sub, path, name);
-        }
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        fprintf(2, "not enough arguments\n");
-        exit(1);
-    }
-
-    char dir[DIRSIZ + 1];
-    char name[DIRSIZ + 1];
-
-    memcpy(dir, argv[1], strlen(argv[1]));
-    memcpy(name, argv[2], strlen(argv[2]));
-
-    int fin;
     struct stat st;
 
-    if ((fin = open(dir, 0)) < 0)
+    fd = open(path, 0);
+
+    if (fd < 0)
     {
-        fprintf(2, "find: cannot open %s\n", dir);
-        exit(1);
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
     }
 
-    if (fstat(fin, &st) < 0)
+    if (fstat(fd, &st) < 0)
     {
-        fprintf(2, "find: cannot stat %s\n", dir);
-        close(fin);
-        exit(1);
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
     }
 
     if (st.type != T_DIR)
     {
-        printf("%s is not a directory\n", dir);
-        close(fin);
-        exit(1);
+        fprintf(2, "find: %s is not a directory\n", path);
+        close(fd);
+        return;
+    }
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof current)
+    {
+        printf("find: path too long\n");
+        close(fd);
+        return;
     }
 
-    find(fin, dir, name);
+    char *ptr;
+    strcpy(current, path);
+    ptr = current + strlen(current);
+    *ptr++ = '/';
 
+    while (read(fd, &de, sizeof(de)) == sizeof(de))
+    {
+        if (de.inum == 0)
+            continue;
+        if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+            continue;
+
+        memmove(ptr, de.name, DIRSIZ);
+        ptr[DIRSIZ] = 0;
+
+        if (stat(current, &st) < 0)
+        {
+            fprintf(2, "find: cannot stat %s\n", current);
+
+            continue;
+        }
+
+        if (st.type == T_FILE && match(de.name, filename))
+        {
+            printf("%s\n", current);
+        }
+        else if (st.type == T_DIR)
+        {
+            find(current, filename); // recursion next into this directory
+            break;
+        }
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(2, "Usage: find path filename\n");
+        exit(1);
+    }
+    if (strcmp(argv[1], ".") == 0)
+    {
+        find(".", argv[2]);
+    }
+    else
+    {
+        find(argv[1], argv[2]);
+    }
     exit(0);
 }
